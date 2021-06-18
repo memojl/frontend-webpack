fcb96c63894f252cff58947a3ab4b3d798b58137
Memo Jimenez <memojl08@gmail.com>
2021-06-18 14:28:47 -0500

    update 18.06.2021

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import { alerta } from './messages.js';
 
+import './style.css';
+
 document.getElementById('btn-alert').addEventListener('click', alerta);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,19 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins:[
     new plugin({
       template: './src/index.html'
